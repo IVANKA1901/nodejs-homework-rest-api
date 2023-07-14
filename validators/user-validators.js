1f9af0e6430b7a10d9a9fe4c83e289dda78a9c5e
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-07-14 19:21:16 +0200

    add schemas for auth

---
+++ validators/user-validators.js
@@ -0,0 +1,22 @@
+const Joi = require("joi");
+
+const emailRegexp = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const subscriptionTypes = ["starter", "pro", "business"];
+
+const userAuthSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required().messages({
+    "any.required": "missing required email field",
+  }),
+  password: Joi.string().required().messages({
+    "any.required": "missing required password field",
+  }),
+  subscription: Joi.string().valid(...subscriptionTypes),
+});
+
+const schemas = {
+  userAuthSchema,
+};
+
+module.exports = {
+  schemas,
+};
